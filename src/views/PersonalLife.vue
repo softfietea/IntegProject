40bdfdcb7c8f20ba36d1aef4240cd75edea4a574
Tribaco <68200121+softfietea@users.noreply.github.com>
2022-10-19 13:55:09 +0800

    Image Fix 1

--- src/views/PersonalLife.vue
+++ src/views/PersonalLife.vue
@@ -1,25 +1,26 @@
 <template>
   <v-container>
     <h1 class="title">Personal Life</h1>
+
     <v-img
       width="400"
       alt=""
       class="hidden-md"
-      src="./src/assets/alfiePicture.jpg"
+      src="../src/assets/alfiePicture.jpg"
     ></v-img>
 
     <br />
     <v-card
       title="About me"
       subtitle="This section talks about my personal life"
-     
       text="   I am Alfie C. Tribaco, 4th year student hoping to be a mobile app dev in
       the future. I was able to finish my senior high school at TIP, my high school at Dona Teodora Alonzo High School, and elementary in Cecilio Apostol Elementary school. I'm 22 years old and got interest with coding or developing something that can solve current problems that our world facing. "
     ></v-card>
   </v-container>
 </template>
 
-<script setup></script>
+<script setup>
+</script>
 
 <style scoped>
 .title {
